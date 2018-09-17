5ba08f3ae6ca25ac529b0dd371f7968835c79bb6
Loris Bettazza <loris.bettazza@gmail.com>
2018-09-17 04:12:50 +0200

    :fire: Removed unused pages /components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/header';
 import './index.css';
 
 const Layout = ({ children, data }) => (
