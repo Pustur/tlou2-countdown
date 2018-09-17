67311132f31ee0539c4ae7b8e499fcd00197874b
Loris Bettazza <loris.bettazza@gmail.com>
2018-09-17 04:15:17 +0200

    :speech_balloon: Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'TLOU Part II - Countdown to release',
+    description: 'The Last of Us Part II - Countdown to release',
+    keywords: [
+      'TLOU',
+      'The Last of Us',
+      'The Last of Us Part II',
+      'The Last of Us Part 2',
+      'Countdown',
+      'Release',
+    ],
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,21 +9,14 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        {
+          name: 'keywords',
+          content: data.site.siteMetadata.keywords.join(', '),
+        },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 );
 
@@ -38,6 +31,8 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
       }
     }
   }
