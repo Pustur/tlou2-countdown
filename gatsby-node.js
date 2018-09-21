7fb99ac97f588ba1b09976bf138c4aa0a5142a45
Loris Bettazza <loris.bettazza@gmail.com>
2018-09-21 05:07:56 +0200

    :wrench: Changed gatsby config to be able to see images in other devices

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+/**
+ * Show images when accessing the site in development mode using other devices
+ * Ref: https://github.com/gatsbyjs/gatsby/issues/5264#issuecomment-405424852
+ */
+if (process.env.NODE_ENV === 'development') {
+  process.env.GATSBY_WEBPACK_PUBLICPATH = '/';
+}
