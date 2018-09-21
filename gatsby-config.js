7835b267fc746bb516eee413b5d9992de903ce4a
Loris Bettazza <loris.bettazza@gmail.com>
2018-09-21 05:20:12 +0200

    :recycle: Removed useless StaticQuery and simplified Layout component
    Also removed keywords meta tag because it's useless in 2018

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  siteMetadata: {
-    title: 'TLOU Part II - Countdown to release',
-    description: 'The Last of Us Part II - Countdown to release',
-    keywords: [
-      'TLOU',
-      'The Last of Us',
-      'The Last of Us Part II',
-      'The Last of Us Part 2',
-      'Countdown',
-      'Release',
-    ],
-  },
   plugins: ['gatsby-plugin-react-helmet'],
 };
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,45 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
 
 import './Layout.css';
 
-const query = graphql`
-  query LayoutQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        keywords
-      }
-    }
-  }
-`;
-
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={query}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: 'description',
-              content: data.site.siteMetadata.description,
-            },
-            {
-              name: 'keywords',
-              content: data.site.siteMetadata.keywords.join(', '),
-            },
-          ]}
-          htmlAttributes={{ lang: 'en-US' }}
-        />
-        {children}
-      </>
-    )}
-  />
+  <>
+    <Helmet
+      title="TLOU Part II - Countdown to release"
+      meta={[
+        {
+          name: 'description',
+          content: 'The Last of Us Part II - Countdown to release',
+        },
+      ]}
+      htmlAttributes={{ lang: 'en-US' }}
+    />
+    {children}
+  </>
 );
 
 Layout.propTypes = {
